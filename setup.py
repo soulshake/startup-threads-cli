611a3fdad4496579dd98844e19bae903007c552d
AJ Bowen <aj@gandi.net>
2015-08-14 18:00:17 -0700

    add README.rst, add arrow dependency

--- setup.py
+++ setup.py
@@ -2,16 +2,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='swag',
-    version='0.1.0',
+    name='startupthreads-cli',
+    version='0.1.11',
     author='AJ Bowen',
+    license='MIT',
     author_email='aj@gandi.net',
     packages=find_packages(),
     description='Startup Threads CLI',
-    long_description=open('README.md').read(),
-    url='',
+    long_description=open('README.rst').read(),
+    url='https://github.com/soulshake/startup-threads-cli',
     keywords='startup threads startupthreads cli swag shirts',
     install_requires=['click==3.3',
+                      'arrow==0.5.4',
                       'requests==2.7.0',
                       'tabulate==0.7.5'],
     entry_points="""\
